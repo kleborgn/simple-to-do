9c8eb4e7ad098558d2e3334dbc69702e07943562
kleborgn <3341364+kleborgn@users.noreply.github.com>
2023-12-17 16:29:15 +0100

    test

--- sw.js
+++ sw.js
@@ -1,21 +1,16 @@
-// Change this to your repository name
-var GHPATH = '/simple-to-do';
-
-// Choose a different app prefix name
-var APP_PREFIX = 'example_';
-
-// The version of the cache. Every time you change any of the files
-// you need to change this version (version_01, version_02…). 
-// If you don't change the version, the service worker will give your
-// users the old files!
-var VERSION = 'version_00';
-
-// The files to make available for offline use. make sure to add 
-// others to this list
-var URLS = [    
-  `${GHPATH}/`,
-  `${GHPATH}/index.html`,
-  `${GHPATH}/styles/base.css`,
-  `${GHPATH}/styles/to-do-styles.css`,
-  `${GHPATH}/simple-to-do.js`
-]
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+  };
+  
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    addResourcesToCache([    
+      "/simple-to-do/",
+      "/simple-to-do/index.html",
+      "/simple-to-do/styles/base.css",
+      "/simple-to-do/styles/to-do-styles.css",
+      "/simple-to-do/simple-to-do.js"
+    ]),
+  );
+  });
